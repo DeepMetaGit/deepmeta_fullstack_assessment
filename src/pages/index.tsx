c7be775fe59583450f0c8d3d1e487a87695bd6bb
Deepak Patidar <deepakpatidar@Deepaks-MacBook-Pro.local>
2023-08-08 12:07:48 +0100

    added counter and button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import BigBox from "~/components/bigBox";
 import SmallBox from "~/components/smallBox";
 
 export default function Home() {
+  const [counter, setcounter] = useState<number>(0);
+
   return (
     <>
       <main>
-        <div className="m-8 flex items-end gap-8">
+        <div className="m-4 text-xl">Counter: {counter}</div>
+        <div className="m-4 flex items-end gap-8">
           <SmallBox />
           <BigBox />
         </div>
+        <button className="m-4 rounded-sm border border-slate-950 bg-slate-200 px-4 py-2">
+          Reset
+        </button>
       </main>
     </>
   );
